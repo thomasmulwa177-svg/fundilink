233e5ec86451c30ea96c7592d78cf1cdc09a8036
thomasmulwa177-svg <thomasmulwa177@gmail.com>
2026-05-21 23:08:37 +0300

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -1,4 +1,25 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+function AdminDashboard() {
+  return <h1>Admin Dashboard</h1>;
+}
+
+function FundiDashboard() {
+  return <h1>Fundi Dashboard</h1>;
+}
+
+function ClientDashboard() {
+  return <h1>Client Dashboard</h1>;
+}
 
 export default function App() {
-  return <h1>FundiLink Kenya</h1>
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/admin" element={<AdminDashboard />} />
+        <Route path="/fundi" element={<FundiDashboard />} />
+        <Route path="/client" element={<ClientDashboard />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
