bc8fd2104e37e4150adb533783adde3ae1fd9fc4
thomasmulwa177-svg <thomasmulwa177@gmail.com>
2026-05-21 22:57:55 +0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,1 +1,18 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 
+const firebaseConfig = {
+  apiKey: "AIzaSyAOa2Btp04MwIby6epGndMbqgu2qIkpIdQ",
+  authDomain: "fundilink-chat.firebaseapp.com",
+  projectId: "fundilink-chat",
+  storageBucket: "fundilink-chat.firebasestorage.app",
+  messagingSenderId: "71381605004",
+  appId: "1:71381605004:web:9c8b12f52715083e3fb9a9",
+  measurementId: "G-1T1R4ZPHHE"
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
+export const db = getFirestore(app);
