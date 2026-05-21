b1f5ae597f24c5bb9ec356084a6b7ddb985af688
thomasmulwa177-svg <thomasmulwa177@gmail.com>
2026-05-21 23:53:35 +0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,18 +1,9 @@
-import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
-import { getFirestore } from "firebase/firestore";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyAOa2Btp04MwIby6epGndMbqgu2qIkpIdQ",
-  authDomain: "fundilink-chat.firebaseapp.com",
-  projectId: "fundilink-chat",
-  storageBucket: "fundilink-chat.firebasestorage.app",
-  messagingSenderId: "71381605004",
-  appId: "1:71381605004:web:9c8b12f52715083e3fb9a9",
-  measurementId: "G-1T1R4ZPHHE"
-};
-
-const app = initializeApp(firebaseConfig);
-
-export const auth = getAuth(app);
-export const db = getFirestore(app);
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
