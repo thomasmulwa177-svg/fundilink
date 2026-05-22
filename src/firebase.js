d062e9a128833d99ae7ecbbcc2b0ecf6d7230191
thomasmulwa177-svg <thomasmulwa177@gmail.com>
2026-05-22 12:44:49 +0300

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -2,20 +2,16 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
-// Firebase config (YOUR REAL DATA)
 const firebaseConfig = {
-  apiKey: "AIzaSyAOa2Btp04MwIby6epGndMbqgu2qIkpIdQ",
-  authDomain: "fundilink-chat.firebaseapp.com",
-  projectId: "fundilink-chat",
-  storageBucket: "fundilink-chat.firebasestorage.app",
-  messagingSenderId: "71381605004",
-  appId: "1:71381605004:web:9c8b12f52715083e3fb9a9",
-  measurementId: "G-1T1R4ZPHHE"
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_AUTH_DOMAIN",
+  projectId: "YOUR_PROJECT_ID",
+  storageBucket: "YOUR_STORAGE_BUCKET",
+  messagingSenderId: "YOUR_SENDER_ID",
+  appId: "YOUR_APP_ID",
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
-// EXPORTS (this is what your error was missing)
 export const auth = getAuth(app);
 export const db = getFirestore(app);
