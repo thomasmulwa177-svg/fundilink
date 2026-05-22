2735f667835624a67453f8294bde1cc335712cf1
thomasmulwa177-svg <thomasmulwa177@gmail.com>
2026-05-22 13:02:14 +0300

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,5 +1,5 @@
 import { initializeApp } from "firebase/app";
-import { getAuth, setPersistence, browserLocalPersistence } from "firebase/auth";
+import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
@@ -15,6 +15,3 @@ const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
 export const db = getFirestore(app);
-
-// 🔥 IMPORTANT: keep user logged in after refresh
-setPersistence(auth, browserLocalPersistence);
