85f3348138035a5a2853a2bd3367b2a12772de81
thomasmulwa177-svg <thomasmulwa177@gmail.com>
2026-05-22 03:13:01 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,9 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-
-import Home from "./pages/Home";
-import Login from "./pages/Login";
-import Register from "./pages/Register";
-import AdminDashboard from "./pages/AdminDashboard";
-import FundiDashboard from "./pages/FundiDashboard";
-import ClientDashboard from "./pages/ClientDashboard";
-
-export default function App() {
+function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/admin" element={<AdminDashboard />} />
-        <Route path="/fundi" element={<FundiDashboard />} />
-        <Route path="/client" element={<ClientDashboard />} />
-      </Routes>
-    </BrowserRouter>
-  );
+    <div>
+      <h1>FundiLink Working Successfully</h1>
+    </div>
+  )
 }
+
+export default App
