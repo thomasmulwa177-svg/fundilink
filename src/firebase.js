1a57c0fde4920cd476aeaf6705acef9cd082ba93
thomasmulwa177-svg <thomasmulwa177@gmail.com>
2026-05-22 04:22:35 +0300

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,1 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAOa2Btp04MwIby6epGndMbqgu2qIkpIdQ",
+  authDomain: "fundilink-chat.firebaseapp.com",
+  projectId: "fundilink-chat",
+  storageBucket: "fundilink-chat.firebasestorage.app",
+  messagingSenderId: "71381605004",
+  appId: "1:71381605004:web:9c8b12f52715083e3fb9a9",
+  measurementId: "G-1T1R4ZPHHE"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
