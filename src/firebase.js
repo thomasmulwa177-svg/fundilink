3f4a58b60f9b2c00f2437b9e8c86debe8c8cb7c4
thomasmulwa177-svg <thomasmulwa177@gmail.com>
2026-05-22 13:14:40 +0300

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,17 +1,11 @@
-import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
-import { getFirestore } from "firebase/firestore";
-
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_AUTH_DOMAIN",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_STORAGE_BUCKET",
-  messagingSenderId: "YOUR_SENDER_ID",
-  appId: "YOUR_APP_ID",
+  apiKey: "AIzaSyAOa2Btp04MwIby6epGndMbqgu2qIkpIdQ",
+  authDomain: "fundilink-chat.firebaseapp.com",
+  databaseURL: "https://fundilink-chat-default-rtdb.firebaseio.com",
+  projectId: "fundilink-chat",
+  storageBucket: "fundilink-chat.firebasestorage.app",
+  messagingSenderId: "71381605004",
+  appId: "1:71381605004:web:9c8b12f52715083e3fb9a9",
+  measurementId: "G-1T1R4ZPHHE"
 };
-
-const app = initializeApp(firebaseConfig);
-
-export const auth = getAuth(app);
-export const db = getFirestore(app);
