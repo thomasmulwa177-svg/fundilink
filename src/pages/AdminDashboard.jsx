611dcdb5dea43c5e9e3cee93a3ee72b127ca6ee2
thomasmulwa177-svg <thomasmulwa177@gmail.com>
2026-05-22 12:50:06 +0300

    Update AdminDashboard.jsx

--- src/pages/AdminDashboard.jsx
+++ src/pages/AdminDashboard.jsx
@@ -1,15 +1,11 @@
 <button
   onClick={async () => {
-    try {
-      const { addDoc, collection } = await import("firebase/firestore");
-      await addDoc(collection(db, "test"), {
-        time: Date.now(),
-      });
-      alert("WRITE SUCCESS");
-    } catch (e) {
-      alert("FAILED: " + e.message);
-      console.log(e);
-    }
+    const { addDoc, collection } = await import("firebase/firestore");
+    await addDoc(collection(db, "test"), {
+      ok: true,
+      time: Date.now(),
+    });
+    alert("WRITE WORKING");
   }}
 >
   TEST WRITE
