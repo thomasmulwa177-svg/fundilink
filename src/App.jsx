4048fad60cba8e2e6d58093b15d4a97588389a61
thomasmulwa177-svg <thomasmulwa177@gmail.com>
2026-05-22 13:55:03 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,3 @@
-import { useEffect, useState } from "react";
-import { auth } from "./firebase";
-import {
-  onAuthStateChanged,
-  signInWithEmailAndPassword,
-  signOut,
-} from "firebase/auth";
-
-export default function App() {
-  const [user, setUser] = useState(null);
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-
-  useEffect(() => {
-    const unsub = onAuthStateChanged(auth, (u) => {
-      setUser(u);
-    });
-
-    return () => unsub();
-  }, []);
-
-  const login = async () => {
-    try {
-      await signInWithEmailAndPassword(auth, email, password);
-    } catch (e) {
-      alert(e.message);
-    }
-  };
-
-  const logout = () => signOut(auth);
-
-  if (!user) {
-    return (
-      <div style={{ padding: 20 }}>
-        <h2>Login</h2>
-
         <input
           placeholder="Email"
           onChange={(e) => setEmail(e.target.value)}
